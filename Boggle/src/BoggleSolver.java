a6d9ed69aa227efeb87c65c3a628ae3fe685c60d
黄小非 <xiaofei_huang@hotmail.com>
2013-12-23 03:54:37 +0800

    Regular commit

--- Boggle/src/BoggleSolver.java
+++ Boggle/src/BoggleSolver.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -9,13 +10,13 @@ import java.util.TreeSet;
 
 public class BoggleSolver
 {
-    private TST<String> dict;
+    private MyTST<String> dict;
     //BoggleBoard board;
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
         //this.dictionary = dictionary;
-        dict = new TST();
+        dict = new MyTST();
         for(int i = 0; i < dictionary.length; i++) {
             this.dict.put(dictionary[i], new String(""+i));
         }
@@ -23,7 +24,7 @@ public class BoggleSolver
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        Set<String> result = new TreeSet<String>();
+        Set<String> result = new HashSet<String>();
         for(int i = 0; i < board.rows(); i++) {
             for(int j = 0; j < board.cols(); j++) {
                 boolean[] marks = new boolean[board.cols() * board.rows()];
@@ -57,7 +58,8 @@ public class BoggleSolver
                 //System.out.println(curWord);
             }
         }
-        if("".equals(dict.prefixMatch(curWord).toString())) return;
+        System.out.println("testing:" + curWord);
+        if(!dict.hasPrefixMatch(curWord)) return;
 
         int nextLevel = (ch == 'Q')?(index + 2):(index + 1);
 
@@ -75,6 +77,8 @@ public class BoggleSolver
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
+        if(word == null) throw new IllegalArgumentException();
+        if(!dict.contains(word)) return 0;
         int length = word.length();
         int score = 0;
         if(length <= 2) score = 0;
@@ -94,10 +98,10 @@ public class BoggleSolver
         BoggleBoard board = new BoggleBoard(args[1]);
         int score = 0;
         for (String word : solver.getAllValidWords(board))
-        {
-            StdOut.println(word);
-            score += solver.scoreOf(word);
-        }
+    {
+        StdOut.println(word);
+        score += solver.scoreOf(word);
+    }
         StdOut.println("Score = " + score);
     }
 }
